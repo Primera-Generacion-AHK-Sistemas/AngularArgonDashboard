019b6a624eac6d939ff2d8af260f491079ae3113
Dreadlocks99 <julyhindu_4@hotmail.com>
2020-09-19 18:26:59 -0300

    ✨Agregado de services/api/python

---
+++ src/app/services/api/python/python-data.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PythonDataService {
+    today: number = Date.now();
+    private baseUrl = 'https://flask-yahoo-fin.herokuapp.com/api/live-price';
+    constructor(private http: HttpClient) {}
+
+    accion(auctionName: string, todayDate: string, aYearAgo: string) {
+        const headers = new HttpHeaders().set(
+            'Content-Type',
+            'application/json'
+        );
+
+        let params = new HttpParams()
+            .set('ticker', auctionName)
+            .set('start_date', aYearAgo)
+            .set('end_date', todayDate);
+        const url = this.baseUrl; // + '/asset/all';
+        return this.http.get(url, { headers: headers, params: params });
+    }
+}
