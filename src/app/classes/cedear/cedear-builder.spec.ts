e3f34a34305becac59f64aa81f5df97df964088b
Dreadlocks99 <julyhindu_4@hotmail.com>
2020-09-26 17:45:15 -0300

    🔥Borrar cedears interface

--- src/app/classes/cedear/cedear-builder.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CedearBuilder } from './cedear-builder';
-
-describe('CedearBuilder', () => {
-  it('should create an instance', () => {
-    expect(new CedearBuilder()).toBeTruthy();
-  });
-});
